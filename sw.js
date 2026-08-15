b84858a032e3d04a5c030252def3916f2681f264
nmaidane-droid <nmaidane@hotmail.com>
2026-08-15 16:10:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Station — Suivi ISS
    Incrémenter VERSION à chaque modification de index.html ou des ressources. */
-const VERSION = "iss-v2";
+const VERSION = "iss-v3";
 const COQUE = [
   "./",
   "./index.html",
